4bbfbf0a92d7951db06c6cadcd4f4d6031e1a9d2
sj.hyeon <uiandwe@gmail.com>
2019-04-09 18:51:35 +0900

    less 삭제 scss 적용 npm install npm run start

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
             loader: "sass-loader"
           }
         ]
-      }
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   }
 };
